377295225c9656bc3c543059fa65d7c435713558
Treven Trujillo <treventrujillo@gmail.com>
2017-09-13 10:44:00 -0600

    revised method name

--- main.rb
+++ main.rb
@@ -1,15 +1,15 @@
 require 'pry'
 require 'colorize'
 
-def check_palindrome(input)
+def check_palindrome?(input)
   input[0] == input[-1] && input[1] == input[-2]
 end
 
 def menu
   puts "Enter a word:"
   input = gets.chomp.downcase
-  check_palindrome(input)
-  palindromic = check_palindrome(input)
+  check_palindrome?(input)
+  palindromic = check_palindrome?(input)
   if palindromic == true
     puts "Word is palindromic".colorize(:green)
   else 
